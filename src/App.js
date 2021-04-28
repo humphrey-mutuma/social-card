874bb4073f2ff18a8eea352ac1d9c3fb0f32665f
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-04-28 11:17:05 +0300

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Avator from './components/AvatorComponent/Avotar'
+import SocialCard from './components/SocialCardComponent/SocialCard'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="Card">
+      <aside className="AvatorComponent">
+        <Avator />
+      </aside>
+      <article className="SocialCardComponent">
+        <SocialCard />
+      </article>
+    </section>
   );
 }
 
---
+++ src/components/AvatorComponent/Avator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Avator.css';
+
+const Avator = () => {
+    return (
+        <div className="Avator">
+            <h1>DEV</h1>
+        </div>
+    )
+};
+
+export default Avator;
